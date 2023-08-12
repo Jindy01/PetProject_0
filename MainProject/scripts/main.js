64ca38cc020a1d1d8cfeb6d0ea47f3c4ef17c467
Jindy01 <vip.fsafds@mail.ru>
2023-08-12 16:08:45 +0300

    EndForProject

--- MainProject/scripts/main.js
+++ MainProject/scripts/main.js
@@ -1,18 +1,18 @@
-'use strict'
+"use strict";
 
-function openPage(event, pageName) {
-    let i, tabcontent, buttons;
+//  Позволяет при наведении переходить по картачкам информации
+function openPage(pageName) {
+  let i, tabcontent, buttons;
 
-    tabcontent = document.getElementsByClassName('tabcontent');
-    for(i = 0; i < tabcontent.length; ++i) {
-        tabcontent[i].style.display = 'none';
-    }
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; ++i) {
+    tabcontent[i].style.display = "none";
+  }
 
-    buttons = document.getElementsByClassName('buttons');
-    for(i = 0; i < buttons.length; ++i) {
-        buttons[i].className = buttons[i].className.replace('active', '');
-    }
-    
-    document.getElementById(pageName).style.display = 'block';
-    event.currentTarget.className += 'active';
-}   
+  buttons = document.getElementsByClassName("buttons");
+  for (i = 0; i < buttons.length; ++i) {
+    buttons[i].className = buttons[i].className.replace("active", "");
+  }
+
+  document.getElementById(pageName).style.display = "block";
+}
